f2104bd72bf02b11c2fdb7e4980582b865a4587e
Alexander Costa Brandão <alexander.estagio@anatel.gov.br>
2022-04-05 15:36:34 -0300

    funcional

---
+++ funcional.rb
@@ -0,0 +1,17 @@
+class Banco
+
+    def initialize(contas)
+        @contas = contas
+    end
+
+    def status
+        saldo = 0
+        for conta in @contas
+            saldo += conta
+        end
+        puts saldo
+    end
+end
+
+banco = Banco.new([200, 300, 400])
+banco.status 
