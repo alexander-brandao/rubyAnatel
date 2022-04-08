9db8a4f54dc6ba4437c4ee9700f36b131d648d2d
Alexander Costa Brandão <alexander.estagio@anatel.gov.br>
2022-04-08 15:46:51 -0300

    programação funcional

---
+++ array.rb
@@ -0,0 +1,14 @@
+#criar uma funcao que adiciona elementos em uma array
+class Vetor
+    
+    def cria_uma_array
+        array = []
+        self.each do |elemento|
+            array << elemento.upcase
+        end
+        array
+    end
+end
+
+
+#pagina 47 manipulando exceptions
