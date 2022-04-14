482e3e5cf1c80f799b537f62503ad91a09cf2a90
Alex <alex12a7x@gmail.com>
2022-04-14 15:19:53 -0300

    metaprogramacao

---
+++ metaprogramacao.rb
@@ -0,0 +1,12 @@
+class Restaurante
+
+    def initialize(nome)
+        puts "criando um novo restaurante: #{nome}"
+        @@total ||= 0
+        @@total = @@total + 1
+        puts "Restaurantes criados: #{@@total}"
+        @nome = nome
+    end
+end
+
+restaurante = Restaurante.new("Giraffas")
--- teste.rb
+++ teste.rb
@@ -1,6 +1,4 @@
-#pagina 51 para saber IO
-print "Escreva um texto: "
-texto = gets
-File.open("promessa.txt", "w") do |f|
-    f << texto
+Dir.entries('caelum.txt').each do |file_name|
+    idea = File.read( file_name )
+    puts idea
 end
